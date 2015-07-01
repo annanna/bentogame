76f6fa3d8212472d890e57bab5f8113fb8801b9b
Anna Muenster <anna.muenster@web.de>
2015-07-01 16:21:14 +0200

    initial screen with falling food items

--- BentoGame/GameScene.m
+++ BentoGame/GameScene.m
@@ -8,42 +8,78 @@
 
 #import "GameScene.h"
 
+
+@interface GameScene ()
+@property (nonatomic) NSTimeInterval lastCreationTimeInterval;
+@property (nonatomic) NSTimeInterval lastUpdateTimeInterval;
+@end
+
 @implementation GameScene
 
 -(void)didMoveToView:(SKView *)view {
+    
+    
     /* Setup your scene here */
-    SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+}
+
+-(void)addFood {
     
-    myLabel.text = @"Hello, World!";
-    myLabel.fontSize = 65;
-    myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                   CGRectGetMidY(self.frame));
+    NSArray *foodObjects = [NSArray arrayWithObjects:@"circle", @"rectangle", @"triangle", nil];
+    int randomVal = arc4random() % 3;
     
-    [self addChild:myLabel];
+    
+    // Create sprite
+    SKSpriteNode *foodItem = [SKSpriteNode spriteNodeWithImageNamed:foodObjects[randomVal]];
+    
+    // position
+    int minX = foodItem.size.width / 2;
+    int maxX = self.frame.size.width - foodItem.size.width / 2;
+    int rangeX = maxX - minX;
+    int actualX = (arc4random() % rangeX) + minX;
+    
+    foodItem.position = CGPointMake(actualX, self.frame.size.height + foodItem.size.height/2);
+    [self addChild:foodItem];
+    
+    
+    // speed
+    int minDuration = 2.0;
+    int maxDuration = 4.0;
+    int actualDuration = (arc4random() & (maxDuration - minDuration)) + minDuration;
+    
+    // actions
+    SKAction *actionMove = [SKAction moveTo:CGPointMake(actualX, -foodItem.size.height/2) duration:actualDuration];
+    SKAction *actionMoveDone = [SKAction removeFromParent];
+    [foodItem runAction:[SKAction sequence:@[actionMove, actionMoveDone]] completion:^{
+        NSLog(@"out of screen");
+    }];    
+}
+
+-(void)updateWithTimeSinceLastUpdate:(CFTimeInterval) timeSinceLast {
+    self.lastCreationTimeInterval += timeSinceLast;
+    if (self.lastCreationTimeInterval > 1) {
+        self.lastCreationTimeInterval = 0;
+        [self addFood];
+    }
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
-    
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.xScale = 0.5;
-        sprite.yScale = 0.5;
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
-    }
 }
 
 -(void)update:(CFTimeInterval)currentTime {
     /* Called before each frame is rendered */
+    // Handle time delta:
+    // if we drop below 60fps, we still want everything to move the same distance
+    CFTimeInterval timeSinceLast = currentTime - self.lastUpdateTimeInterval;
+    self.lastUpdateTimeInterval = currentTime;
+    
+    if (timeSinceLast > 1) {
+        // more than a second since the last update
+        timeSinceLast = 1.0 / 60.0;
+        self.lastUpdateTimeInterval = currentTime;
+    }
+    
+    [self updateWithTimeSinceLastUpdate:timeSinceLast];
 }
 
 @end
